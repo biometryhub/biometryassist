3d9d01926074b6126e181c48524df2b4857cddda
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2022-03-31 13:45:50 +1030

    Disabling warning message in logl.test. Fixes #17

--- tests/testthat/test-logltest.R
+++ tests/testthat/test-logltest.R
@@ -14,13 +14,10 @@ test_that("function works", {
                             resid.terms = c("ar1(Row)", "ar1(Column)"), decimals = 1, quiet = TRUE)
 
     expect_equal(oats.logl$Term, c("Blocks", "Blocks:Wplots", "ar1(Row)", "ar1(Column)"))
-    expect_equal(oats.logl$LogLRT.pvalue, c("0.05795", "0.13142", "0.00559", "0.82896"))
-    expect_equal(oats.logl2$LogLRT.pvalue, c(0.05795, 0.13142, 0.00559, 0.82896))
+    expect_equal(oats.logl$LogLRT.pvalue, c("0.05795", "0.13142", "0.00559", "0.82883"))
+    expect_equal(oats.logl2$LogLRT.pvalue, c(0.05795, 0.13142, 0.00559, 0.82883))
     expect_true(is.numeric(oats.logl2$LogLRT.pvalue))
     expect_equal(oats.logl3$LogLRT.pvalue, c('0.1', '0.1', '<0.1', '0.8'))
-    expect_warning(logl_test(model.obj = model.asr, rand.terms = c("Blocks", "Blocks:Wplots"),
-                             resid.terms = c("ar1(Row)", "ar1(Column)"), decimals = 5),
-                   "Model did not converge")
 })
 
 test_that("logltest gives an error on different model type", {
