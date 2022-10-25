e82c049e142dba455d86be7db0e27b8494eec036
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2022-10-25 16:34:12 +1030

    Skipping tests on Win/Mac

--- tests/testthat/test-all-w2.r
+++ tests/testthat/test-all-w2.r
@@ -389,9 +389,10 @@ test_that("exercise 13 works", {
   skip_on_cran()
   skip_if(packageVersion("grid") < "4.2.1")
   vdiffr::expect_doppelganger(title = "exercise13resplot", resplot(exercise13.asr))
-  vdiffr::expect_doppelganger(title = "exercise13variogram", variogram(exercise13.asr))
   vdiffr::expect_doppelganger(title = "exercise13autoplot1", autoplot(pred13e.out1))
   vdiffr::expect_doppelganger(title = "exercise13autoplot2", autoplot(pred13e.out2))
+  skip_on_os(c("windows", "mac"))
+  vdiffr::expect_doppelganger(title = "exercise13variogram", variogram(exercise13.asr))
 })
 
 test_that("exercise 14 works", {
@@ -422,8 +423,9 @@ test_that("exercise 14 works", {
   skip_on_cran()
   skip_if(packageVersion("grid") < "4.2.1")
   vdiffr::expect_doppelganger(title = "exercise14resplot", resplot(exercise14.asr))
-  vdiffr::expect_doppelganger(title = "exercise14variogram", variogram(exercise14.asr))
   vdiffr::expect_doppelganger(title = "exercise14autoplot", autoplot(pred14e.out))
+  skip_on_os(c("windows", "mac"))
+  vdiffr::expect_doppelganger(title = "exercise14variogram", variogram(exercise14.asr))
 })
 
 test_that("exercise 15 works", {
@@ -455,9 +457,10 @@ test_that("exercise 15 works", {
   skip_on_cran()
   skip_if(packageVersion("grid") < "4.2.1")
   vdiffr::expect_doppelganger(title = "exercise15resplot", resplot(exercise15.asr))
-  vdiffr::expect_doppelganger(title = "exercise15variogram", variogram(exercise15.asr))
   vdiffr::expect_doppelganger(title = "exercise15autoplot1", autoplot(pred15e.out1))
   vdiffr::expect_doppelganger(title = "exercise15autoplot2", autoplot(pred15e.out2))
   vdiffr::expect_doppelganger(title = "exercise15autoplot3", autoplot(pred15e.out3))
+  skip_on_os(c("windows", "mac"))
+  vdiffr::expect_doppelganger(title = "exercise15variogram", variogram(exercise15.asr))
 })
 
