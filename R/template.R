48a21c93d7f40e2709d0bd87bf60bfda7c02116d
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2023-06-29 15:00:42 +0930

    Adding template for aov

--- R/template.R
+++ R/template.R
@@ -12,9 +12,17 @@
 #' \dontrun{
 #' analysis_template(filename = "test_report")
 #' }
-analysis_template <- function(filename) {
-    file <- file.path(paste0(filename, ifelse(tools::file_ext(filename)!="R", ".R", "")))
-    file.copy(system.file("analysis_template.R", package = "biometryassist"), file)
-    utils::file.edit(file)
+analysis_template <- function(filename, asreml = ifelse(rlang::is_installed("asreml"), TRUE, FALSE)) {
+    if(asreml) {
+        file <- file.path(paste0(filename, ifelse(tools::file_ext(filename)!="R", ".R", "")))
+        file.copy(system.file("analysis_template.R", package = "biometryassist"), file)
+        utils::file.edit(file)
+    }
+    else {
+        file <- file.path(paste0(filename, ifelse(tools::file_ext(filename)!="R", ".R", "")))
+        file.copy(system.file("analysis_template_aov.R", package = "biometryassist"), file)
+        utils::file.edit(file)
+    }
+    message("Replace all values given in <angle brackets>")
     invisible(file)
 }
