cacb806dc4e4cb3d5ad6b441d4bbd8a385e79ee7
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2024-02-28 21:44:37 +1030

    Creating buffers from partial data frames

--- R/create_buffers.R
+++ R/create_buffers.R
@@ -12,51 +12,70 @@ create_buffers <- function(design, type, blocks = FALSE) {
 
     # Match edge, edges or e
     if(grepl("(^edges?$|^e$)", tolower(type))) {
+        design$row <- design$row+1
+        design$col <- design$col+1
+
+        min_row <- min(design$row)
+        min_col <- min(design$col)
+
         row <- c(rep(1, ncol+2), rep(nrow+2, ncol+2), rep(2:(nrow+1), 2))
         col <- c(rep(1:(ncol+2), 2), rep(1, nrow), rep(ncol+2, nrow))
         n_brow <- length(row)  # Number of buffer rows
         treatments <- rep("buffer", n_brow)
-
-        design$row <- design$row+1
-        design$col <- design$col+1
     }
     # Match row, rows, r
     else if(grepl("(^rows?$|^r$)", tolower(type))) {
-        row <- rep(seq(1, (2*nrow)+1, by = 2), each = ncol)
+        design$row <- 2*design$row
+
+        min_row <- min(design$row)
+        min_col <- min(design$col)
+
+        row <- rep(seq(min_row, (2*nrow)+1, by = 2), each = ncol)
         col <- rep(seq(1, ncol),times = nrow+1)
         n_brow <- length(row)  # Number of buffer rows
         treatments <- rep("buffer", n_brow)
-        design$row <- 2*design$row
     }
     # Match col, cols, column, columns or c
     else if(grepl("(^col(umn)?s?$|^c$)", tolower(type))) {
+        design$col <- 2*design$col
+
+        min_row <- min(design$row)
+        min_col <- min(design$col)
+
         row <- rep(seq(1, nrow), times = ncol+1)
-        col <- rep(seq(1, (2*ncol)+1, by = 2), each = nrow)
+        col <- rep(seq(min_col, (2*ncol)+1, by = 2), each = nrow)
         n_brow <- length(row)  # Number of buffer rows
         treatments <- rep("buffer", n_brow)
-        design$col <- 2*design$col
     }
     # Match double row, double rows, or dr
     else if(grepl("(^double rows?$|^dr$)", tolower(type))) {
-        row <- c(rep(seq(1, (3*nrow)-2, by = 3),
+        design$row <- (3*design$row)-1
+
+        min_row <- min(design$row)
+        min_col <- min(design$col)
+
+        row <- c(rep(seq(min_row-1, (3*nrow)-2, by = 3),
                      each = ncol),
-                 rep(seq(3, (3*nrow), by = 3),
+                 rep(seq(min_row+1, (3*nrow), by = 3),
                      each = ncol))
-        col <- rep(seq(1, ncol), times = 2*nrow)
+        col <- seq(min_col, ncol)
         n_brow <- length(row)  # Number of buffer rows
         treatments <- rep("buffer", n_brow)
-        design$row <- (3*design$row)-1
     }
     # Match double col, double cols, double column, double columns, dc
     else if(grepl("(^double col(umn)?s?$|^dc$)", tolower(type))) {
-        row <- rep(seq(1, nrow), times = 2*ncol)
-        col <- c(rep(seq(1, (3*ncol)-2, by = 3),
+        design$col <- (3*design$col)-1
+
+        min_row <- min(design$row)
+        min_col <- min(design$col)
+
+        row <- seq(min_row, nrow)
+        col <- c(rep(seq(min_col-1, (3*ncol)-2, by = 3),
                      each = nrow),
-                 rep(seq(3, (3*ncol), by = 3),
+                 rep(seq(min_col+1, (3*ncol), by = 3),
                      each = nrow))
         n_brow <- length(row)  # Number of buffer rows
         treatments <- rep("buffer", n_brow)
-        design$col <- (3*design$col)-1
     }
     # Match block, blocks, or b
     else if(grepl("(^blocks?$|^b$)", tolower(type))) {
