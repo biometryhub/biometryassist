637d08be62209f6521972c03595e000c159a5b27
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2024-05-30 12:40:08 +0930

    Updates to summary_graph

--- R/summary_graph.R
+++ R/summary_graph.R
@@ -16,40 +16,45 @@
 #' summary_graph(iris, "Petal.Length", "Species", "mm")
 #' summary_graph(npk, "yield", c("N", "P", "K"), "lb/plot")
 #'
-summary_graph <- function(data, response, exp_var, resp_units, alpha = 0.3){
-    # What if data is not present?
-    # What if data is not a data frame?
-    # What if response is misspelt?
-    # What if response is an invalid type?
-    # What if >2 exp_var?
-    #
+summary_graph <- function(data, response, exp_var, resp_units){
 
-    if(length(exp_var)==1){
-        gg <- ggplot2::ggplot(data = data, ggplot2::aes(x = .data[[exp_var]], y = .data[[response]])) +
-            ggplot2::geom_boxplot() +
-            ggplot2::geom_point(alpha = alpha) +
-            ggplot2::labs(y = paste(response, " (", resp_units, ")", sep = "")) +
-            ggplot2::theme_bw()
+    # TODO: NSE
+
+    if(!is.data.frame(data)) {
+        stop(data, " is not a data frame.", call. = FALSE)
     }
-    else if(length(exp_var) == 2){
+    if(response %!in% colnames(data)) {
+        stop(response, " does not appear to be a column of data. Please check input.", call. = FALSE)
+    }
+    if(!is.numeric(data[[response]])) {
+        warning(response, " is not a numeric variable.")
+    }
+
+    if(length(exp_var)==1) {
+        gg <- ggplot2::ggplot(data = data, ggplot2::aes(x = .data[[exp_var]], y = .data[[response]])) +
+            ggplot2::geom_boxplot()
+    }
+    else if(length(exp_var) == 2) {
         gg <- ggplot2::ggplot(data = data, ggplot2::aes(x = .data[[exp_var[1]]], y = .data[[response]],
                                                         colour = .data[[exp_var[2]]], group = .data[[exp_var[2]]])) +
             ggplot2::stat_summary(fun = mean, geom = "point") +
             ggplot2::stat_summary(fun = mean, geom = "line") +
-            ggplot2::geom_point(alpha = alpha) +
-            ggplot2::labs(y = paste(response, " (", resp_units, ")", sep = "")) +
-            ggplot2::theme_bw()
+            ggplot2::geom_point(alpha = 0.3)
+    }
+    else if(length(exp_var)==3) {
+        gg <- ggplot2::ggplot(data = data, ggplot2::aes(x = .data[[exp_var[1]]], y = .data[[response]],
+                                                        colour = .data[[exp_var[2]]], group = .data[[exp_var[2]]])) +
+            ggplot2::stat_summary(fun = mean, geom = "point") +
+            ggplot2::stat_summary(fun = mean, geom = "line") +
+            ggplot2::facet_wrap(~ .data[[exp_var[3]]])
     }
     else {
-        gg <- ggplot2::ggplot(data = data, ggplot2::aes(x = .data[[exp_var[1]]], y = .data[[response]],
-                                                        colour = .data[[exp_var[2]]], group = .data[[exp_var[2]]])) +
-            ggplot2::stat_summary(fun = mean, geom = "point") +
-            ggplot2::stat_summary(fun = mean, geom = "line") +
-            ggplot2::facet_wrap(~ .data[[exp_var[3]]]) +
-            ggplot2::geom_point(alpha = alpha) +
-            ggplot2::labs(y = paste(response, " (", resp_units, ")", sep = "")) +
-            ggplot2::theme_bw()
+        stop("Additional explanatory variables are not currently handled.")
     }
+
+    gg <- gg + ggplot2::geom_point(alpha = 0.3) +
+        ggplot2::labs(y = paste(response, " (", resp_units, ")", sep = "")) +
+        ggplot2::theme_bw()
 
     return(gg)
 }
