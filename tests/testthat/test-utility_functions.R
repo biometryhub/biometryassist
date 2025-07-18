271f678eb6aff3e1cbba020654a78c718e5927df
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2025-07-18 13:17:22 +0930

    Trying to mock crayon being unavailable

--- tests/testthat/test-utility_functions.R
+++ tests/testthat/test-utility_functions.R
@@ -10,7 +10,7 @@ test_that("Package message prints on load", {
 
 test_that("Output prints if crayon is not installed", {
     rlang::local_interactive(value = TRUE)
-    mockery::stub(biometryassist:::.onAttach, "rlang::is_installed", function(...) FALSE)
+    local_mocked_bindings(is_installed = function(...) FALSE)
     expect_output(print(biometryassist:::.onAttach(pkg = "biometryassist")))
 })
 
