686b2cb9053a83c79657a7950b8d63f6b1a6879b
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2025-10-01 22:34:02 +0930

    Testing missed lines

--- tests/testthat/test-design.R
+++ tests/testthat/test-design.R
@@ -599,6 +599,13 @@ test_that("autoplot responds to size argument", {
                                 autoplot(d1, size = 8), variant = ggplot2_variant())
 })
 
+test_that("autoplot responds to legend argument", {
+    d1 <- design(type = "crd", treatments = c(1, 5, 10, 20),
+                 reps = 5, nrows = 4, ncols = 5, seed = 42, quiet = TRUE)
+    vdiffr::expect_doppelganger(title = "autoplot with size",
+                                autoplot(d1, legend = FALSE), variant = ggplot2_variant())
+})
+
 test_that("Colour blind friendly plots work", {
     # CRD
     d1 <- design("crd", treatments = LETTERS[1:11], reps = 4,
